0de8f6bb25127b6c9f5bc41a880f993948d9773f
Michał Dobranowski <mdbrnowski@gmail.com>
2025-12-20 18:35:25 +0100

    Add functional equation example

---
+++ case_studies/FunctionalEquation.lean
@@ -0,0 +1,29 @@
+import Mathlib.Data.PNat.Basic
+import Mathlib.Tactic.Linarith
+
+
+example {f : ℕ+ → ℕ+} : (∀ m n : ℕ+, m * m + f n ∣ m * f m + n) ↔ f = id := by
+  constructor
+  · intro h
+    have f2_eq_2 : f 2 = 2 := by
+      specialize h 2 2
+      obtain ⟨k, hk⟩ := exists_eq_mul_left_of_dvd (PNat.dvd_iff.mp h)
+      have k_eq_1 : k = 1 := by
+        nlinarith
+      apply PNat.eq
+      nlinarith
+    funext x
+    rw [id_eq]
+    refine le_antisymm ?_ ?_
+    · specialize h 2 x
+      rw [f2_eq_2] at h
+      have := PNat.le_of_dvd h
+      simp at this
+      assumption
+    · specialize h x x
+      have := PNat.le_of_dvd h
+      change (x * x + f x : ℕ) ≤ x * f x + x at this
+      change (x : ℕ) ≤ f x
+      nlinarith [PNat.pos x, PNat.pos (f x)]
+  · intro h
+    simp [h]
