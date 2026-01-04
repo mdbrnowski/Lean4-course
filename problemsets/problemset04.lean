e635f2e6686ad9fc41ab991cc8161720ca58c8cf
Michał Dobranowski <mdbrnowski@gmail.com>
2026-01-04 11:09:01 +0100

    Add second problem (problemset04)

--- problemsets/problemset04.lean
+++ problemsets/problemset04.lean
@@ -6,3 +6,7 @@ import Mathlib.Data.Finite.Defs
 -- Udowodnić, że każdy skończony pierścień całkowity jest ciałem.
 theorem finite_integral_domain_is_field (R : Type*) [CommRing R] [IsDomain R] [Finite R] :
     IsField R := sorry
+
+-- Zadanie 2
+-- Podpowiedź: użyj Nat.le_induction
+example {n : ℕ} (hn : 2 ≤ n) : 3^n > n * 2^n := sorry
